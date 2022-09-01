2de2e3be6f864e20d501b07ed488d1f3bcfcc55d
Mustafa Gönültaş <73774639+gonultasmf@users.noreply.github.com>
2022-09-01 18:58:31 +0300

    Add files via upload

---
+++ ColorMaker/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using System.Diagnostics;
+
+namespace ColorMaker;
+
+public partial class MainPage : ContentPage
+{
+    bool isRandom = false;
+    string hexValue = "#000000";
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        if (!isRandom)
+        {
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+        }
+    }
+
+    private void SetColor(Color color)
+    {
+        Debug.WriteLine(color.ToString());
+        btnRandom.BackgroundColor = color;
+        Container.BackgroundColor = color;
+        hexValue = color.ToHex();
+        lblHex.Text = hexValue;
+    }
+
+    private void btnRandom_Clicked(object sender, EventArgs e)
+    {
+        isRandom = true;
+        var random = new Random();
+        Color color = Color.FromRgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+        SetColor(color);
+        sldRed.Value = color.Red;
+        sldGreen.Value = color.Green;
+        sldBlue.Value = color.Blue;
+        isRandom = false;
+    }
+
+    private async void imgBtnCopy_Clicked(object sender, EventArgs e)
+    {
+        await Clipboard.SetTextAsync(hexValue);
+        var toast = Toast.Make("Color cpoied",
+            CommunityToolkit.Maui.Core.ToastDuration.Short, 12);
+        await toast.Show();
+    }
+}
+
